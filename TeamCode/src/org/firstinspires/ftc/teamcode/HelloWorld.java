975672c60b04964558ebaef8645be3cb72b9efcd
ridambhatia-ftc <ridam.bhatia@oarobotics.org>
2025-09-04 22:33:47 -0700

    Added HelloWorld.java

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous()
+public class HelloWorld extends OpMode {
+    @Override
+    public void init(){
+        String name = "ridam";
+        telemetry.addData("Hello",name);
+    }
+
+    @Override
+    public void loop(){}
+}
